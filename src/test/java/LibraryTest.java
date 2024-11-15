29bf010b75ddc4f7d912b8f73623e4e5b053ccea
Oleg Somov <doremi@fasollasi.ru>
2024-11-15 15:26:57 +0300

    try jenkins

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LibraryTest {
